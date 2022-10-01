0de2118c75c3963d74cc4e54bf4e0c62d3132643
Luis Retana <luisretana123@gmail.com>
2022-10-01 03:47:00 -0400

    added mongo connection to index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,18 @@ require("dotenv").config();
 
 app.use(express.json());
 
+const connectToMongo = async (req, res) => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connected successfully to MongoDB`)
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1)
+    }
+}
+
+connectToMongo();
+
 
 
 const PORT = process.env.PORT || 8080;
