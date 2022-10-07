2ea289b7d7aaab8c3ab02cc5419560c4e44186fc
Luis Retana <luisretana123@gmail.com>
2022-10-07 02:42:47 -0400

    added comments and cors functionality

--- Backend/index.js
+++ Backend/index.js
@@ -3,14 +3,20 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
+const cors = require("cors");
 
-app.use(express.json());
+
+app.use(express.json());// parses incoming JSON requests
+app.use(cors());//allows server to take requests.
 
 const connectToMongo = async (req, res) => {
+    //attempt to connect to MongoDB
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log(`Connected successfully to MongoDB`)
-    } catch (err) {
+    }
+    //error is thrown for unsuccessful connection and exit out of the server. 
+    catch (err) {
         console.error(err.message);
         process.exit(1)
     }
@@ -18,7 +24,6 @@ const connectToMongo = async (req, res) => {
 
 connectToMongo();
 
-
-
+//Setting Port to a variable for readability
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, console.log(`Listening on port: ${PORT}`))
