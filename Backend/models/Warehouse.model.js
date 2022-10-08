2ed3c3e6a81233a220a8e0463a69c115c8dc481f
Luis Retana <luisretana123@gmail.com>
2022-10-08 07:30:06 -0400

    added warehouseschema skeleton

--- Backend/models/Warehouse.model.js
+++ Backend/models/Warehouse.model.js
@@ -1,7 +1,10 @@
-const mongoose = reqquire("mongoose");
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const wareHouseSchema = new Schema({});
+const wareHouseSchema = new Schema({
+    warehouse_id: Number,
+    warehouse_availability: Number
+});
 
-const Warehouse = mongoose.model("Warehouse", wareHouseSchema, "Warehouse")
+const Warehouse = mongoose.model("Warehouse", wareHouseSchema, "Warehouses")
 module.exports = Warehouse;
