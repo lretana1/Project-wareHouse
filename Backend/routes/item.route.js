585e587b477455430d5d26b7e620f46ace4a780c
Luis Retana <luisretana123@gmail.com>
2022-10-08 08:21:34 -0400

    added working delete method

--- Backend/routes/item.route.js
+++ Backend/routes/item.route.js
@@ -37,4 +37,13 @@ router.put("/:id", async (req, res) => {
     }
 });
 
+router.delete("/:id", async (req, res) => {
+    try {
+        await deleteById(req.params.id);
+        res.send("hello");
+    } catch (err) {
+        res.status(err?.status ?? 500).json(err)
+    }
+});
+
 module.exports = router;
