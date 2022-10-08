971a60ced51033b4df803182c0d87f1b4352cd7d
Luis Retana <luisretana123@gmail.com>
2022-10-08 06:17:06 -0400

    adding get method to find all warehouses

--- Backend/routes/warehouse.route.js
+++ Backend/routes/warehouse.route.js
@@ -1,6 +1,15 @@
 const router = require("express").Router;
 const { findWarehouse, createWarehouse } = require("../controllers/warehouse.controller");
 
+router.length("/", async (req, res) => {
+    try {
+        const warehouse = await findWarehouse();
+        res.status(200).json(warehouse);
+    } catch (err) {
+        res.status(500), json(err);
+    }
+});
+
 router.post("/", async (req, res) => {
     try {
         const warehouse = await createWarehouse(req.body);
