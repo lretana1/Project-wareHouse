85e7e703617efb10db714478a2d6ddaf9c188f21
Luis Retana <luisretana123@gmail.com>
2022-10-09 20:02:17 -0400

    updated item schema

--- Backend/models/item.model.js
+++ Backend/models/item.model.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const itemSchema = new Schema({
-    item_id: String,
-    description: String,
+    name: String,
     quantity: Number,
     price: Number,
-    warehouse_id: String//references warehouse
 });
 
 const Item = mongoose.model('Item', itemSchema, 'Items');
