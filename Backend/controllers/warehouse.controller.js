4dcd4d6f55fa9e8205109b6b89c56c2decf0df85
Luis Retana <luisretana123@gmail.com>
2022-10-09 23:33:48 -0400

    added fucntionality for items in inventory

--- Backend/controllers/warehouse.controller.js
+++ Backend/controllers/warehouse.controller.js
@@ -1,8 +1,22 @@
 const Warehouse = require("../models/warehouse.model.js");
 
-const findWarehouse = async () => await Warehouse.find().populate("");
+const findWarehouse = async () => await Warehouse.find().populate("inventory.item");
 
-const createWarehouse = async wareHouseCreation => {
+const findWarehouseById = async (id) => {
+    try {
+        const warehouse = await Warehouse.findById(id).populate('inventory.item');
+        if (warehouse == null) throw { status: 204, msg: `Item:${id}, does not exist` };
+        return warehouse;
+    } catch (err) {
+        throw { status: 500, msg: err.message };
+    }
+}
+
+const removeItemById = async (id) => {
+    await Warehouse.findByIdAndDelete(id);
+}
+
+const createWarehouse = async (wareHouseCreation) => {
     try {
 
         const wareHouse = new Warehouse(wareHouseCreation);
@@ -13,4 +27,71 @@ const createWarehouse = async wareHouseCreation => {
         throw { status: 500, msg: err.message };
     }
 };
-module.exports = { findWarehouse, createWarehouse }
+
+
+const updateWarehouse = async (id, warehouseToUpdate) => {
+    try {
+        return await Warehouse.findByIdAndUpdate(id, warehouseToUpdate);
+    } catch (err) {
+        throw { status: 400, msg: err }
+    }
+}
+
+const insertToWarehouse = async (id, itemToInsert) => {
+    try {
+        const item = { "_id": itemToInsert.item._id }
+        const inventory = await Warehouse.findOneAndUpdate({ _id: id },
+            {
+                "$addToSet": {
+                    "inventory": {
+                        item: item,
+                        quantity: itemToInsert.quantity
+                    },
+                }, "$inc": {
+                    "remaining": -itemToInsert.quantity
+                }
+            })
+    } catch (err) {
+        throw { status: 400, msg: err }
+    }
+}
+
+const removeItemFromWarehouse = async (warehouseId, id, data) => {
+    try {
+        const inventory = await Warehouse.findOneAndUpdate({ _id: warehouseId },
+            {
+                "$pull": {
+                    "inventory": {
+                        item: id,
+                    },
+                }, "$inc": {
+                    "remaining": data.quantity
+                }
+            })
+    } catch (err) {
+        throw { status: 400, msg: err }
+    }
+}
+
+const updateWarehouseItem = async (id, itemToUpdate) => {
+    try {
+        const ID2 = itemToUpdate.item._id
+        const inventory = await Warehouse.updateOne({
+            'inventory._id': ID2
+        },
+            {
+                "$set": {
+                    "inventory.$.quantity": itemToUpdate.quantity
+
+                }, "$inc": {
+                    "remaining": itemToUpdate.addOrMinus
+                }
+            })
+        return inventory;
+    } catch (err) {
+        throw { status: 400, msg: err }
+    }
+}
+
+
+module.exports = { findWarehouse, createWarehouse, findWarehouseById, removeItemById, updateWarehouse, updateWarehouseItem, removeItemFromWarehouse, insertToWarehouse }
